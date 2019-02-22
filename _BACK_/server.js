5aaa4b2e628ef19e76242daba81c5bfc9e765ebc
Davy Parys <davyparys45@gmail.com>
2019-02-22 11:49:42 +0100

    add jason back folder

--- _BACK_/server.js
+++ _BACK_/server.js
@@ -2,7 +2,7 @@ const X = require('express');
 const APP = X();
 const PORT = 1045;
 
-APP.use('/SITE', X.static(__dirname + '/_SITE_/'));
+APP.use('/', X.static(__dirname + '/_SITE_/'));
 
 APP.listen(PORT, () => {
     console.log(`/r/nNODE ::: I started my back-end server.`);
